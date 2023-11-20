9b036f6b64249865464480afd48d21d9c71690b5
AlinaKuvaiskova <144594576+AlinaKuvaiskova@users.noreply.github.com>
2023-11-20 14:59:08 +0300

    Add files via upload

---
+++ занятие_1/8 (1занятие).py
@@ -0,0 +1,6 @@
+name = input('Ваши фамилия, имя?: ')
+age = input('Сколько вам лет?: ')
+place = input('Где вы живёте?: ')
+print(f'Ваши фамилия, имя {name}')
+print(f'Ваш возраст: {age}')
+print(f'Вы живете в {place}')
